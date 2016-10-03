9389987298df9a64c337051a184a647b1ba2528a
macchi92 <nicolamacchitella@MacBook-Pro-di-nicola-2.local>
2016-10-03 13:25:14 -0400

    3rd ass

---
+++ PUI2016_nm2762/HW4_nm2762/Assignment_2.py
@@ -0,0 +1,105 @@
+
+# coding: utf-8
+
+# # Chi square 1
+
+# article: http://journals.plos.org/plosone/article?id=10.1371/annotation/ea14adcb-033d-492d-8f8b-e047aa080cd4
+# 
+# 
+# IV(s): 282 Y-chromosomes
+# 
+# IV type(s): Categorical
+# 
+# 
+# 
+# DV(s): haplogroups
+# 
+# DV type(s): Categorical	
+# 
+# 
+# 
+# Control Var	: Ladin and Grecani Salentini:
+# 
+# Control Var type :	categorical
+# 
+# 
+# 
+# Question to be answered: Is there a correlation in Contemporary Italian Population with Its Pre-Roman Heritage?
+# 
+# 
+# 
+# H0: The correlation in Contemporary Italian Population with Its Pre-Roman Heritage is equal to or less than The correlation in Contemporary Italian Population with other population's heritage
+# 
+# 
+# 
+# alpha: 0.05
+# 
+#      
+# 
+# 
+
+# # Anova
+
+# article: http://journals.plos.org/plosone/article?id=10.1371/journal.pone.0081193
+# 
+# IV(s): Placenta and membranes from premature born babies
+# 
+# IV type(s): Categorical
+# 
+# 
+# DV(s): Histological chorioamnionitis presence
+# 
+# DV type(s): Categorical
+# 
+# 
+# Control Var : Non premature born babies placenta and membranes
+# 
+# Control Var type : categorical
+# 
+# 
+#  
+# H0: The babies that are non premature born and developed histological chorioamnionitis presence is equal to or bigger than the number of of premature born babies with the same patology.
+# 
+# 
+# alpha: 0.05
+# 
+# 
+# Question to be answered: Is there a correlation between prematurity, gender and chorioamnionitis as determinants of early life lung function in premature infants?
+
+# # Multiple Regression
+
+# article: http://journals.plos.org/plosone/article?id=10.1371/journal.pone.0145435
+# 
+# 
+# IV(s): 2, Income and benefits
+# 
+# IV type(s): ordinal
+# 
+# 
+# DV(s): 8, EDSS, Age, Sex, Geographical region, Family Composition, Type of living area, Contry of birth, Education
+# 
+# DV type(s): Categorical
+# 
+# 
+# Control Var : 0
+# 
+# 
+# Control Var type : none
+# 
+# 
+# H0: People with disability have lower income and higher benefits than people without.
+# 
+# alpha: 0.05
+# 
+# 
+# Question to be answered: How are income and benefits correlated with disability
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
